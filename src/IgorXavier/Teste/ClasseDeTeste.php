3bbe12b0ac87b9ef9a182cda33cc0ef225e618b6
vagrant <vagrant@local.phpteste>
2015-06-01 03:57:41 +0000

    Commit inicial para teste de ciração de pacotes

---
+++ src/IgorXavier/Teste/ClasseDeTeste.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace IgorXavier\Teste;
+
+/**
+ * Classe de teste
+ */
+class ClasseDeTeste {
+	private static $teste = "Apenas uma frase de teste";
+
+	public static function Testar() {
+		echo $this->teste;
+	}
+}
